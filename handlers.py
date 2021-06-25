496d5c00ac55ba78ffa6f0c9113bd5a71bf398dc
Alexander <pythonjsgo@gmail.com>
2021-06-25 03:24:16 +0300

    + CreateFileLinkHandler

--- handlers.py
+++ handlers.py
@@ -1,30 +1,37 @@
-from functools import wraps
 import telegram
 from telegram import Update, ReplyKeyboardMarkup, ChatAction, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CallbackContext
+from telegram.utils import helpers
+import uuid
 
 import config
+import tools
 from db_manager import DB_manager
-from services.mail_traking_notifycations import short_report, full_report
+from tools import send_typing_action
 
-db_manager = DB_manager()
+bot = telegram.Bot(token=config.TOKEN)
+db = DB_manager()
 bot = telegram.Bot(token=config.TOKEN)
 
 
-def send_typing_action(func):
-    """Sends typing action while processing func command."""
+@send_typing_action
+def GetFileHandler(update: Update, context: CallbackContext):
+    # b.write_user_position(update.message.chat_id, "main")
 
-    @wraps(func)
-    def command_func(update, context, *args, **kwargs):
-        context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.TYPING)
-        return func(update, context,  *args, **kwargs)
+    print(f"New message from {update.message.chat_id} , text : {update.message.text}")
 
-    return command_func
+    user_id = update.message.from_user.id
 
 
-
-
-
+def CreateFileLinkHandler(update: Update, context: CallbackContext):
+    print(f"New message from {update.message.chat_id} , text : {update.message.text}")
+    user_id = update.message.from_user.id
+    if user_id in config.admin_list:
+        update.message.reply_document(update.message.document.file_id)
+        uuid = tools.get_random_string(10)
+        db.add_file_id(update.message.document.file_id, uuid)
+        deep_linked_url = helpers.create_deep_linked_url(bot.username, uuid)
+        update.message.reply_text("Link " + deep_linked_url)
 
 
 @send_typing_action
@@ -32,89 +39,43 @@ def StartHandler(update: Update, context: CallbackContext):
     # b.write_user_position(update.message.chat_id, "main")
 
     print(f"New message from {update.message.chat_id} , text : {update.message.text}")
-
-
     user_id = update.message.from_user.id
-    if not db_manager.user_exist(user_id): db_manager.add_user(user_id)
-    db_manager.update_user_position(user_id, "StartHandler")
-    db_manager.debug_info()
+    db.update_user_position(user_id, "StartHandler")
+    if update.message.text != "/start":
+        print(update.message.text[7:])
+        keyboard = [
+            [
+                InlineKeyboardButton("Проверить подписку", callback_data=update.message.text[7:]),
+            ],
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
 
-    keyboard = [
-        [telegram.KeyboardButton("Добавить отправление ➕")],
-        [telegram.KeyboardButton("Мои отправления ✉")]
-    ]
-
-    reply_markup = ReplyKeyboardMarkup(keyboard)
-    update.message.reply_text('Пожалуйста выберете!', reply_markup=reply_markup)
-
-@send_typing_action
-def AddDeparture(update: Update, context: CallbackContext):
-    # b.write_user_position(update.message.chat_id, "main")
-    print(f"New message from {update.message.chat_id} , text : {update.message.text}")
-
-
-    user_id = update.message.from_user.id
-    db_manager.update_user_position(user_id, "AddDeparture")
-    db_manager.debug_info()
-
-    keyboard = [
-        [telegram.KeyboardButton("Назад ◀️")],
-    ]
-
-    reply_markup = ReplyKeyboardMarkup(keyboard)
-    update.message.reply_text("Трек номер:", reply_markup=reply_markup)
-
-@send_typing_action
-def MyDepartures(update: Update, context: CallbackContext):
-    print(f"New message from {update.message.chat_id} , text : {update.message.text}")
-
-
-    user_id = update.message.from_user.id
-    db_manager.update_user_position(user_id, "MyDepartures")
-    db_manager.debug_info()
-
-    keyboard = [
-        [telegram.KeyboardButton("Назад ◀️")],
-    ]
-
-    if len(db_manager.get_user_departures(user_id)) == 0:
-        departures = "Нет отправлений"
-    else:
-        departures = db_manager.get_user_departures(user_id)
-
-    reply_markup = ReplyKeyboardMarkup(keyboard)
-    update.message.reply_text(departures, reply_markup=reply_markup)
-
-@send_typing_action
-def Other(update: Update, context: CallbackContext):
-    # b.write_user_position(update.message.chat_id, "main")
-
-    user_id = update.message.from_user.id
-    position = db_manager.get_user_position(user_id)[0]
-    db_manager.debug_info()
-    if position == "AddDeparture":
-        response = short_report(update.message.text)
-        if response != "Трек код не найден":
-
-            db_manager.add_user_departures(user_id, update.message.text, response)
-            keyboard = [
-                [
-                    InlineKeyboardButton("Полный отчет", callback_data='full_report|'+update.message.text),
-                ]
-            ]
-            reply_markup = InlineKeyboardMarkup(keyboard)
-            update.message.reply_text(response, reply_markup=reply_markup)
+        status = bot.getChatMember(chat_id=config.channel_id, user_id=user_id).status
+        if status == "creator" or status == "administrator" or status == "member":
+            print(db.get_file_id(update.message.text[7:])[0])
+            update.message.reply_document(db.get_file_id(update.message.text[7:])[0])
         else:
-            bot.send_message(update.message.chat_id, response)
+            update.message.reply_text(f"Для получения файла подпишитесь на канал {config.channel_name}",
+                                      reply_markup=reply_markup)
 
-    print(f"New message from {update.message.chat_id} , text : {update.message.text}")
+
+    else:
+        if user_id in config.admin_list:
+            update.message.reply_text("Приветствую, Администратор! ")
+            update.message.reply_text('Отправьте файл, чтобы получить ссылку')
 
 
 def button(update: Update, context: CallbackContext):
     query = update.callback_query
+    print(query.data)
+    user_id = query.from_user.id
+    status = bot.getChatMember(chat_id=-1001589540427, user_id=user_id).status
+    if status == "creator" or status == "administrator" or status == "member":
+        print(db.get_file_id(query.data)[0])
+        bot.send_document(chat_id=user_id, document=db.get_file_id(query.data)[0])
+    else:
+        bot.send_message(chat_id=user_id, text="Подписка не найдена!")
 
     # CallbackQueries need to be answered, even if no notification to the user is needed
     # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
     query.answer()
-    if query.data.split("|")[0] == "full_report":
-        query.edit_message_text(text=full_report(query.data.split("|")[1]))
--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 import logging
-
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 
 import config
-from db_manager import DB_manager
-from handlers import StartHandler, AddDeparture, MyDepartures, Other, button
+from handlers import StartHandler, button, GetFileHandler, CreateFileLinkHandler
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-db_manager = DB_manager()
 
 def main():
     updater = Updater(config.TOKEN, use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', StartHandler))
-    dispatcher.add_handler(MessageHandler(Filters.regex("Добавить отправление ➕"), AddDeparture))
-    dispatcher.add_handler(MessageHandler(Filters.regex("Назад ◀️"), StartHandler))
-    dispatcher.add_handler(MessageHandler(Filters.regex("Мои отправления ✉"), MyDepartures))
-    dispatcher.add_handler(MessageHandler(Filters.text, Other))
+    dispatcher.add_handler(MessageHandler(Filters.document, CreateFileLinkHandler))
     updater.dispatcher.add_handler(CallbackQueryHandler(button))
 
     updater.start_polling()
