66531ecc9d8514d8bae6ddab45eb03e3d434a78a
Alexander <pythonjsgo@gmail.com>
2021-06-27 18:37:22 +0300

    admin_list refactored

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 TOKEN = "1811726861:AAEXT4o707Cs13Jh_2w08tazg41xu0xWukc"
 
-admin_list = [882079062]
 
+
+admin_list = eval(open("admins.txt").read())   #someshitcode
 channel_name = "@botfilesendertest"
 channel_id = -1001589540427
